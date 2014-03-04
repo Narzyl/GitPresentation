d638507140de48e7ddac62aa24fec31e9558d173
Adrien Barrau <adrien.barrau@erni-espana.es>
2014-03-04 18:41:13 +0100

    Second Commit

---
+++ TestLocalization/Controllers/TestToEraseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestLocalization.Controllers
+{
+    public class TestToEraseController : Controller
+    {
+        //
+        // GET: /TestToErase/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
